ff3dcadddd077efc4c22fa736212f0ef0e5e3d78
DuresameenShafi <duresameen32@gmail.com>
2024-11-02 00:33:07 -0700

    Firebase_steup

--- Frontend/configs/FirebaseConfig.jsx
+++ Frontend/configs/FirebaseConfig.jsx
@@ -6,12 +6,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7aN5ISZIhlL6hgHIL0jW_RHEJ7O_09ss",
-  authDomain: "face-detection-3c2bf.firebaseapp.com",
-  projectId: "face-detection-3c2bf",
-  storageBucket: "face-detection-3c2bf.firebasestorage.app",
-  messagingSenderId: "348867929261",
-  appId: "1:348867929261:web:e249bf8de385a5cbc21224"
+
 };
 
 
